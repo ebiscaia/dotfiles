544f7252841f5c093eb9203d819d3e1dcbd4c373
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2026-03-04 18:03:23 +1100

    Required file options.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,6 +41,7 @@ vim.pack.add({
     { src = "https://github.com./Pocco81/DAPInstall.nvim" },
     { src = "https://github.com/ahmedkhalf/project.nvim" },
 })
+require("eddie.options")
 
 -- plugin setup
 -- UI
