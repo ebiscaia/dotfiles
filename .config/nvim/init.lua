4ce40b6e64f6e82ec2aeebea51e54585aa317b69
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2026-03-05 17:41:39 +1100

    Moved contents to "keybindings.lua" file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,3 @@
--- Some keybindings
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>Q", ":wq<CR>")
-vim.keymap.set("n", "<leader>r", ":update<CR> :source<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
-
 -- Packages
 vim.pack.add({
     -- first dependencies for other packages
